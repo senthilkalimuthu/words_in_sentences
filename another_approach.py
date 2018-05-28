d01c2e7464280fb8f35244f2058908995ae25182
Senthil <senthil.kalimuthu@tarento.com>
2018-05-28 13:37:16 +0530

    Fixed sentence terminator being treated as another word

--- another_approach.py
+++ another_approach.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
 
@@ -8,11 +9,11 @@ def parse_args():
 
 
 def main():
-    # data = input()
+    data = input()
     args = parse_args()
     sentence_terminators = ('.', '!', '?')
-    with open(args.file) as f:
-        data = f.readlines()
+    # with open(args.file) as f:
+    #     data = f.readlines()
 
     abbreviations = ['Mr.', 'Mrs.', 'Ms.', 'Dr.', 'Sr.', 'i.e.', 'e.g.']  # etc.,
 
@@ -20,14 +21,21 @@ def main():
     sentence_number = 1
 
     for word in data.split(' '):
+            end_of_line = False
+            if word.endswith(sentence_terminators) and word not in abbreviations:
+                word = word[:-1]
+                end_of_line = True
+
             if word in visited_words.keys():
                 if sentence_number not in visited_words[word]:
                     visited_words[word].append(sentence_number)
             else:
                 visited_words[word] = [sentence_number]
 
-            if word.endswith(sentence_terminators) and word not in abbreviations:
+            if end_of_line:
                 sentence_number += 1
+
+
 
 
     for word, sentence_number in visited_words.items():
